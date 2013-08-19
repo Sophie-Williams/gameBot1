361848f1dba8f66ee9efae13b01d12d7f85b70ac
Shreyas B P <shprabhu@zynga.com>
2013-08-20 01:54:11 +0530

    Cleanup of the main class Adding basic UI functionality to FortManager

--- src/Bot2/Bot2.java
+++ src/Bot2/Bot2.java
@@ -4,12 +4,8 @@ import android.os.RemoteException;
 import android.util.Log;
 
 import com.Fort.FortManager;
-import com.Fort.PreGame;
 import com.android.uiautomator.core.UiDevice;
-import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
-import com.android.uiautomator.core.UiScrollable;
-import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 import com.my.AppHandler.AppHandler;
 
@@ -17,18 +13,16 @@ public class Bot2 extends UiAutomatorTestCase{
 	private static UiDevice m_uiDevice ;
 	private static int DISPLAY_WIDTH;
 	private static int DISPLAY_HEIGHT;
-	private static int START_SCREEN = 1;
-	private static int START_X = 400;
-	private static int START_Y = 360;
 	public void testDemo() throws UiObjectNotFoundException {
 		
 		initSetup();
+		//debug();
 		AppHandler.beginApp("com.droidhen.fortconquer", "Fort");
-		FortManager.begin();
 		debug();
+	    FortManager.begin();
 		exit();
-		//mainFlow();
 	}
+	
 	private void exit() {
 		try{
 			m_uiDevice.unfreezeRotation();	
@@ -41,7 +35,7 @@ public class Bot2 extends UiAutomatorTestCase{
 
 	private void initSetup() {
 		m_uiDevice = getUiDevice();
-		m_uiDevice.pressHome();
+		//m_uiDevice.pressHome();
 		try{
 			m_uiDevice.freezeRotation();	
 			Log.i("BOT: ", " Freeze rotation succeeded");
@@ -50,64 +44,19 @@ public class Bot2 extends UiAutomatorTestCase{
 			Log.i("BOT: ", " Freeze rotation failed");
 		}
 	}
+	
 	private void debug() {
 		DISPLAY_HEIGHT= m_uiDevice.getDisplayHeight();
 		DISPLAY_WIDTH= m_uiDevice.getDisplayWidth();
 		Log.i("BOT: ", String.valueOf(DISPLAY_HEIGHT) + " " + String.valueOf(DISPLAY_WIDTH) );
+		/*getDevice().swipe(300, 300, 420 ,750, 100);
+		getDevice().swipe(300, 300, 420, 760, 100);
+		getDevice().swipe(300, 300, 420, 770,  100);
+		getDevice().swipe(300, 300, 420, 780, 100);*/
 	}
 	
 	public static UiDevice getDevice() {
 		return m_uiDevice;
 	}
-	private boolean openGame(){
-		UiScrollable appViews = new UiScrollable(new UiSelector()
-        .scrollable(true));
-		appViews.setAsHorizontalList();
-		UiObject gameApp;
-		
-		try {
-		gameApp = appViews.getChildByText(new UiSelector()
-        .className(android.widget.TextView.class.getName()), 
-        "Fort Conquer");
-		gameApp.clickAndWaitForNewWindow();
-		
-		} catch(UiObjectNotFoundException e) {
-			Log.i("BOT: ", "Press recent Apps failed due to remote exception");
-			return false;
-		}
-		int c = 10;
-		while ( (c--)!=0 ) {
-			sleep (5);
-			if(m_uiDevice.waitForWindowUpdate(null, 20000)) {
-				Log.i("BOT: ", "Wait for window update done " + String.valueOf(c) );
-			} else {
-				Log.i("BOT: ", "Wait for window update failed "  + String.valueOf(c));
-			}
-		}
-		DISPLAY_HEIGHT= m_uiDevice.getDisplayHeight();
-		DISPLAY_WIDTH= m_uiDevice.getDisplayWidth();
-		Log.i("BOT: ", String.valueOf(DISPLAY_WIDTH) + " " + String.valueOf(DISPLAY_HEIGHT) );
-
-		if(currentWindowType(START_SCREEN) == START_SCREEN) {
-			m_uiDevice.click(400, 360);
-			Log.i("BOT: ", "Clicked start" );
-
-			sleep(2000);
-			m_uiDevice.click(200, 360);
-			Log.i("BOT: ", "Clicked local start" );
-			sleep(2000);
-			m_uiDevice.click (700, 360);
-			Log.i("BOT: ", "Clicked begin battle" );
-			sleep(1000);
-			return true;
-		}
-		return false;
-	}
-	private int currentWindowType(int def){
-		return def;
-	}
-	
-	
-
 
 }
--- src/com/Fort/FortManager.java
+++ src/com/Fort/FortManager.java
@@ -7,8 +7,18 @@ public class FortManager {
 	private static int WINDOW_UPDATE_TIMEOUT = 20000;
 	private static FortManager m_instance;
 	private static int START_1_X = 400, START_1_Y = 360;
+	private static int START_LOCAL_1_X = 200, START_LOCAL_1_Y = 360;
+	private static int START_BATTLE_X = 750, START_BATTLE_Y = 420;
+	
 	public FortManager(){
 		getToStartScreen2();
+		boolean local = true;
+		if(local) {
+			startLocalFight();	
+		}else {
+			startArenaFight();
+		}
+		
 	}
 	public static void begin(){
 		m_instance = new FortManager();
@@ -27,6 +37,36 @@ public class FortManager {
 		Log.i("BOT: ", "At start screen 1" );
 		//getting to start screen 2
 		Bot2.getDevice().click(START_1_X, START_1_Y);
+		if(	Bot2.getDevice().waitForWindowUpdate(null, WINDOW_UPDATE_TIMEOUT)){
+			Log.i("BOT: ", "Start screen 2 done " );
+		}else {
+			Log.i("BOT: ", "Start screen 2 failed ");
+		}		return true;
+	}
+	
+	private boolean startLocalFight() {
+		//For now assuming that I am already on the screen
+		Bot2.getDevice().click(START_LOCAL_1_X, START_LOCAL_1_Y);
+		Bot2.getDevice().waitForWindowUpdate(null, WINDOW_UPDATE_TIMEOUT);
+		if(	Bot2.getDevice().waitForWindowUpdate(null, WINDOW_UPDATE_TIMEOUT)){
+			Log.i("BOT: ", "Unit selection screen done " );
+		}else {
+			Log.i("BOT: ", "Unit selection screen failed " );
+		}		startBattle();
+		return true;
+	}
+	
+	private boolean startBattle(){
+		Bot2.getDevice().click(START_BATTLE_X, START_BATTLE_Y);
+		if(	Bot2.getDevice().waitForWindowUpdate(null, WINDOW_UPDATE_TIMEOUT)){
+			Log.i("BOT: ", "Fight screen done " );
+		}else {
+			Log.i("BOT: ", "Fight screen failed " );
+		}
+		Bot2.getDevice().dumpWindowHierarchy(null);
+		return true;
+	}
+	private boolean startArenaFight() {
 		return true;
 	}
 }
